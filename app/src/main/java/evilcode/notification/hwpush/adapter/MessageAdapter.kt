83a583022cd4ff349354800ffa734392d672e024
icodecho <9eeks.lover@gmail.com>
2026-04-18 21:56:07 +0800

    fixed

--- app/src/main/java/evilcode/notification/hwpush/adapter/MessageAdapter.kt
+++ app/src/main/java/evilcode/notification/hwpush/adapter/MessageAdapter.kt
@@ -5,6 +5,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
