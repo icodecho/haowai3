6cb08a4108f08ba9260db1d1c0f03c45b7b77804
icodecho <9eeks.lover@gmail.com>
2026-04-18 23:43:47 +0800

    fixed

--- app/src/main/java/evilcode/notification/hwpush/HaoWaiApplication.kt
+++ app/src/main/java/evilcode/notification/hwpush/HaoWaiApplication.kt
@@ -2,12 +2,16 @@ package evilcode.notification.hwpush
 
 import android.app.Application
 import evilcode.notification.hwpush.util.LogManager
+import evilcode.notification.hwpush.util.NotificationHelper
+import evilcode.notification.hwpush.util.TokenManager
 
 class HaoWaiApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         LogManager.init(this)
+        TokenManager.init(this)
+        NotificationHelper.createNotificationChannel(this)
         LogManager.i("HaoWaiApplication", "Application started")
     }
 }
